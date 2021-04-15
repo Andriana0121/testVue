d1215d456410989ab98d179c6dc253f35620dddd
Andriana0121 <56000335+Andriana0121@users.noreply.github.com>
2021-04-15 15:27:30 +0300

    Update app.js

--- first/calc/app.js
+++ first/calc/app.js
@@ -19,16 +19,23 @@ const app1 = new Vue({
 const app2= new Vue({
     el: '#app3',
     data: {
-        JS: 1,
-        Python: 0.7,
-        Java: 1.3,
-        Experienta: undefined,
-        salariu: undefined
-        
+       limbaj:{
+            JS: 1,
+            Python: 0.7,
+            Java: 1.3
+            },
+        Experienta: {
+                1: 10000,
+                2: 12000,
+                3: 13500,
+                4: 14000,
+                5: 15000
+            },
+        //salariu: null
     },
     computed: {
         salariu(){
-
+            return this.coeficient*this.experience;
         }
     },
 })
